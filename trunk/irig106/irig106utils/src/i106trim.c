674b5947d6ee22fedd35200ecb09722cfab97abd
bbaggerman <bbaggerman@133a2ce8-d050-0410-a33c-a8f588bb09b4>
2006-07-29 12:46:07 +0000

    Changed copyright notice Ported to i106lib

--- trunk/irig106/irig106utils/src/i106trim.c
+++ trunk/irig106/irig106utils/src/i106trim.c
@@ -1,68 +1,64 @@
 /*==========================================================================
 
-  FFTRIM.C - A program to trim Ver 3 data files based on start and/or
+  I106TRIM.C - A program to trim Ch 10 data files based on start and/or
   stop times.
 
-   Copyright (c) 2005
-   Georgia Tech Research Corporation
-   Atlanta, GA 30332-0415
-  
-   All rights reserved.
-  
-   Redistribution and use in source and binary forms, with or without 
-   modification, are permitted provided that the following conditions are 
-   met:
-  
-     * Redistributions of source code must retain the above copyright 
-       notice, this list of conditions and the following disclaimer.
-  
-     * Redistributions in binary form must reproduce the above copyright 
-       notice, this list of conditions and the following disclaimer in the 
-       documentation and/or other materials provided with the distribution.
-  
-     * Neither the name of the Georgia Tech Applied Research Corporation 
-       nor the names of its contributors may be used to endorse or promote 
-       products derived from this software without specific prior written 
-       permission.
-  
-   This software is provided by the copyright holders and contributors 
-   "as is" and any express or implied warranties, including, but not 
-   limited to, the implied warranties of merchantability and fitness for 
-   a particular purpose are disclaimed. In no event shall the copyright 
-   owner or contributors be liable for any direct, indirect, incidental, 
-   special, exemplary, or consequential damages (including, but not 
-   limited to, procurement of substitute goods or services; loss of use, 
-   data, or profits; or business interruption) however caused and on any 
-   theory of liability, whether in contract, strict liability, or tort 
-   (including negligence or otherwise) arising in any way out of the use 
-   of this software, even if advised of the possibility of such damage.
+ Copyright (c) 2006 Irig106.org
 
-  Created 8 July 1998 by Bob Baggerman
+ All rights reserved.
 
-  $RCSfile: i106trim.c,v $
-  $Date: 2006-01-10 19:55:52 $
-  $Author: irig106 $
-  $Revision: 1.1 $
+ Redistribution and use in source and binary forms, with or without 
+ modification, are permitted provided that the following conditions are 
+ met:
+
+   * Redistributions of source code must retain the above copyright 
+     notice, this list of conditions and the following disclaimer.
+
+   * Redistributions in binary form must reproduce the above copyright 
+     notice, this list of conditions and the following disclaimer in the 
+     documentation and/or other materials provided with the distribution.
+
+   * Neither the name Irig106.org nor the names of its contributors may 
+     be used to endorse or promote products derived from this software 
+     without specific prior written permission.
+
+ This software is provided by the copyright holders and contributors 
+ "as is" and any express or implied warranties, including, but not 
+ limited to, the implied warranties of merchantability and fitness for 
+ a particular purpose are disclaimed. In no event shall the copyright 
+ owner or contributors be liable for any direct, indirect, incidental, 
+ special, exemplary, or consequential damages (including, but not 
+ limited to, procurement of substitute goods or services; loss of use, 
+ data, or profits; or business interruption) however caused and on any 
+ theory of liability, whether in contract, strict liability, or tort 
+ (including negligence or otherwise) arising in any way out of the use 
+ of this software, even if advised of the possibility of such damage.
+
+ Created by Bob Baggerman
+
+ $RCSfile: i106trim.c,v $
+ $Date: 2006-07-29 12:46:07 $
+ $Revision: 1.2 $
 
   ==========================================================================*/
 
 #include <stdio.h>
 #include <conio.h>
-//#include <alloc.h>
-//#include <mem.h>
 #include <time.h>
-#include <dos.h>
+//#include <dos.h>
 
-#include "blockio.h"
-#include "ffdata.h"
+#include "stdint.h"
+
+#include "irig106ch10.h"
+#include "i106_decode_time.h"
 
 /*
  * Macros and definitions
  * ----------------------
  */
 
-#define MAJOR_VERSION  "03"
-#define MINOR_VERSION  "02"
+#define MAJOR_VERSION  "01"
+#define MINOR_VERSION  "00"
 
 #if !defined(bTRUE)
 #define bTRUE   (1==1)
@@ -84,6 +80,9 @@
  * -----------
  */
 
+void          * m_pvBuff     = NULL;
+unsigned long   m_ulBuffSize = 0L;
+
 /*
  * Function prototypes
  * -------------------
@@ -96,122 +95,114 @@ void vUsage(void);
 
 /* ======================================================================== */
 
-
-
 int main (int argc, char *argv[])
-  {
-  SuFfdHandle      tFFD_In;    // Input data file handle
-  SuFfdHandle      tFFD_Out;   // Output data file handle
-  unsigned short   usMsgType;
-  SuFfdrTime       tTime;
-  unsigned short   usBuffSize;
-  void *           pvBuff = NULL;
-
-#if   defined(__BORLANDC__)
-  struct date      tPCDate;
-  struct time      tPCTime;
-#else
-  struct tm       *ptPCDateTime;
-#endif
-
-  int              iArgIdx;
-  int              iInHour;
-  int              iInMin;
-  int              iInSec;
-
-  long             lWriteMsgs = 0L;
-
-  long             lStartTimeOffset = -1L;
-  long             lStopTimeOffset  = -1L;
-  long             lStartTime = -1L;
-  long             lStopTime  = -1L;
-
-  int              iStatus;
+    {
+    int              iI106_In;    // Input data file handle
+    int              iI106_Out;   // Output data file handle
+    unsigned long   ulIrigTime;
 
 
-  // Make sure things stay on UTC
-  putenv("TZ=GMT0");
-  tzset();
+
+    unsigned short   usMsgType;
+    SuFfdrTime       tTime;
+    unsigned short   usBuffSize;
+    void *           pvBuff = NULL;
+
+    struct tm       *ptPCDateTime;
+
+    int              iArgIdx;
+    int              iInHour;
+    int              iInMin;
+    int              iInSec;
+
+    long             lWriteMsgs = 0L;
+
+    long             lStartTimeOffset = -1L;
+    long             lStopTimeOffset  = -1L;
+    long             lStartTime = -1L;
+    long             lStopTime  = -1L;
+
+    int              iStatus;
+
+
+    // Make sure things stay on UTC
+    putenv("TZ=GMT0");
+    tzset();
 
 /*
  * Process the command line arguments
  */
 
-  if (argc < 2) {
-    vUsage();
-    return 1;
-    }
+    if (argc < 2) 
+        {
+        vUsage();
+        return 1;
+        }
 
-  if (argc == 2) {
-    vStats(argv[1]);
-    return 0;
-    }
+    if (argc == 2) 
+        {
+        vStats(argv[1]);
+        return 0;
+        }
 
-  for (iArgIdx=3; iArgIdx<argc; iArgIdx++) {
+    for (iArgIdx=3; iArgIdx<argc; iArgIdx++) 
+        {
 
-    switch (argv[iArgIdx][0]) {
+        switch (argv[iArgIdx][0]) 
+            {
 
-      case '+' :
-        iStatus = sscanf(argv[iArgIdx],"+%d:%d:%d",&iInHour,&iInMin,&iInSec);
-        if (iStatus != 3) {
-          vUsage();
-          return 1;
-          }
-        lStartTimeOffset = SECS(iInHour, iInMin, iInSec);
-        break;
+            case '+' :
+                iStatus = sscanf(argv[iArgIdx],"+%d:%d:%d",&iInHour,&iInMin,&iInSec);
+                if (iStatus != 3) 
+                    {
+                    vUsage();
+                    return 1;
+                    }
+                lStartTimeOffset = SECS(iInHour, iInMin, iInSec);
+                break;
 
-      case '-' :
-        iStatus = sscanf(argv[iArgIdx],"-%d:%d:%d",&iInHour,&iInMin,&iInSec);
-        if (iStatus != 3) {
-          vUsage();
-          return 1;
-          }
-        lStopTimeOffset = SECS(iInHour, iInMin, iInSec);
-        break;
+            case '-' :
+                iStatus = sscanf(argv[iArgIdx],"-%d:%d:%d",&iInHour,&iInMin,&iInSec);
+                if (iStatus != 3) 
+                    {
+                    vUsage();
+                    return 1;
+                    }
+                lStopTimeOffset = SECS(iInHour, iInMin, iInSec);
+                break;
 
-      } /* end command line arg switch */
-    } /* end for all arguments */
+            } /* end command line arg switch */
+        } /* end for all arguments */
 
 /*
  * Open the input file
  */
 
-  // Open the input data file
-  usBuffSize = 0;
-  iStatus = FfdOpen(&tFFD_In, argv[1], EnBioMode_READ, usBuffSize);
-  if (iStatus != EnFfdStatus_OK) {
-    printf ("\nERROR %d opening input file\n",iStatus);
-    return 1;
-    }
+    // Open the input data file
+    enStatus = enI106Ch10Open(&iI106Ch10Handle, argv[1], I106_READ);
+    if (enStatus != I106_OK)
+        {
+        printf("Error opening data file : Status = %d\n", enStatus);
+        return 1;
+        }
 
-  // Make the input data buffer
-  pvBuff = malloc(READ_BUFF_SIZE);
+// DO THIS IN READ LOOP
+    // Make the input data buffer
+    pvBuff = malloc(READ_BUFF_SIZE);
 
-  // Read the first data packet to get the start time
-  usBuffSize = (unsigned short)READ_BUFF_SIZE;
-  iStatus = FfdReadNextMessage(&tFFD_In, &usMsgType, &tTime, &usBuffSize, pvBuff);
+    // Find and read the first data packet to get the start time
 
-  // Set start and stop times
-#if   defined(__BORLANDC__)
-  unixtodos(tTime.lTimeMS, &tPCDate, &tPCTime);
-  tPCTime.ti_hour = 0;
-  tPCTime.ti_min  = 0;
-  tPCTime.ti_sec  = 0;
-  if (lStartTimeOffset != -1) {
-    lStartTime = dostounix(&tPCDate, &tPCTime) + lStartTimeOffset;
-    } // end set start time
-  if (lStopTimeOffset != -1) {
-    if (lStopTimeOffset < lStartTimeOffset)
-      lStopTimeOffset += 60L*60L*24L;
-    lStopTime = dostounix(&tPCDate, &tPCTime) + lStopTimeOffset;
-    } // end set stop time
-#else
+
+
+
+    // Set start and stop times
 // Watch out for time zone in mktime();
-  ptPCDateTime = gmtime(&tTime.lTimeMS);
-  ptPCDateTime->tm_hour = 0;
-  ptPCDateTime->tm_min  = 0;
-  ptPCDateTime->tm_sec  = 0;
-  if (lStartTimeOffset != -1) {
+    ptPCDateTime = gmtime(&tTime.lTimeMS);
+    ptPCDateTime->tm_hour = 0;
+    ptPCDateTime->tm_min  = 0;
+    ptPCDateTime->tm_sec  = 0;
+    if (lStartTimeOffset != -1) 
+    {
     lStartTime = mktime(ptPCDateTime) + lStartTimeOffset;
     } // end set start time
   if (lStopTimeOffset != -1) {
@@ -220,7 +211,6 @@ int main (int argc, char *argv[])
     lStopTime = mktime(ptPCDateTime) + lStopTimeOffset;
     } // end set stop time
 
-#endif
 
   // Reset the data file to the beginning
   FfdClose(&tFFD_In);
@@ -366,8 +356,8 @@ void vStats(char *szFileName)
 
 void vUsage(void)
   {
-  printf("FFTRIM "MAJOR_VERSION"."MINOR_VERSION" "__DATE__" "__TIME__"\n");
-  printf("Usage: fftrim <infile> <outfile> [+hh:mm:ss] [-hh:mm:ss]\n");
+  printf("I106TRIM "MAJOR_VERSION"."MINOR_VERSION" "__DATE__" "__TIME__"\n");
+  printf("Usage: i106trim <infile> <outfile> [+hh:mm:ss] [-hh:mm:ss]\n");
   printf("  +hh:mm:ss - Start copy time\n");
   printf("  -hh:mm:ss - Stop copy time\n");
   printf("Or:    fftrim <infile> to get stats\n");
@@ -375,4 +365,47 @@ void vUsage(void)
   }
 
 
+
+/* ------------------------------------------------------------------------ */
+
+// Find the first IRIG time message
+
+int iFindFirstTime(int iI106Handle)
+
+        
+    while (1==1) 
+        {
+
+        // Read the next header
+        enStatus = enI106Ch10ReadNextHeader(iI106Ch10Handle, &suI106Hdr);
+
+        if (enStatus != I106_OK)
+            {
+            return enStatus;
+            }
+
+        // Make sure our buffer is big enough, size *does* matter
+        if (m_ulBuffSize < suI106Hdr.ulDataLen)
+            {
+            m_pvBuff = realloc(m_pvBuff, suI106Hdr.ulDataLen);
+            m_ulBuffSize = suI106Hdr.ulDataLen;
+            }
+
+        // Read the next data buffer
+        ulReadSize = ulBuffSize;
+        enStatus = enI106Ch10ReadNextData(iI106Ch10Handle, &ulBuffSize, pvBuff);
+        if (enStatus != I106_OK)
+            {
+            printf(" Error reading header : Status = %d\n", enStatus);
+            break;
+            }
+
+        } // end while searching for time packet
+
+    return 1;
+    } // end iFindFirstTime()
+
+
+
+/* ------------------------------------------------------------------------ */
 
