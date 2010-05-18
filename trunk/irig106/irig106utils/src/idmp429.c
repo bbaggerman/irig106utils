d9df5e3ed0c3628a4ec10c53be66228341041cb4
bbaggerman <bbaggerman@133a2ce8-d050-0410-a33c-a8f588bb09b4>
2010-05-18 14:10:38 +0000

    ARINC 429 data dumper

---
+++ trunk/irig106/irig106utils/src/idmp429.c
@@ -0,0 +1,493 @@
+/* 
+
+ idmp429 - A utility for dumping IRIG 106 Ch 10 ARINC 429 data
+
+ Copyright (c) 2010 Irig106.org
+
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without 
+ modification, are permitted provided that the following conditions are 
+ met:
+
+   * Redistributions of source code must retain the above copyright 
+     notice, this list of conditions and the following disclaimer.
+
+   * Redistributions in binary form must reproduce the above copyright 
+     notice, this list of conditions and the following disclaimer in the 
+     documentation and/or other materials provided with the distribution.
+
+   * Neither the name Irig106.org nor the names of its contributors may 
+     be used to endorse or promote products derived from this software 
+     without specific prior written permission.
+
+ This software is provided by the copyright holders and contributors 
+ "as is" and any express or implied warranties, including, but not 
+ limited to, the implied warranties of merchantability and fitness for 
+ a particular purpose are disclaimed. In no event shall the copyright 
+ owner or contributors be liable for any direct, indirect, incidental, 
+ special, exemplary, or consequential damages (including, but not 
+ limited to, procurement of substitute goods or services; loss of use, 
+ data, or profits; or business interruption) however caused and on any 
+ theory of liability, whether in contract, strict liability, or tort 
+ (including negligence or otherwise) arising in any way out of the use 
+ of this software, even if advised of the possibility of such damage.
+
+ Created by Bob Baggerman
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <assert.h>
+
+#include "config.h"
+#include "stdint.h"
+#include "irig106ch10.h"
+
+#include "i106_time.h"
+#include "i106_decode_time.h"
+#include "i106_decode_arinc429.h"
+#include "i106_decode_tmats.h"
+
+
+/*
+ * Macros and definitions
+ * ----------------------
+ */
+
+#define MAJOR_VERSION  "01"
+#define MINOR_VERSION  "00"
+
+#if !defined(bTRUE)
+#define bTRUE   (1==1)
+#define bFALSE  (1==0)
+#endif
+
+
+/*
+ * Data structures
+ * ---------------
+ */
+
+
+/*
+ * Module data
+ * -----------
+ */
+
+int           m_iVersion;    // Data file version
+int           m_usMaxBuffSize;
+
+int           m_iI106Handle;
+
+
+/*
+ * Function prototypes
+ * -------------------
+ */
+
+void vPrintTmats(SuTmatsInfo * psuTmatsInfo, FILE * ptOutFile);
+void vUsage(void);
+
+
+/* ------------------------------------------------------------------------ */
+
+int main(int argc, char ** argv)
+  {
+
+    char                    szInFile[256];     // Input file name
+    char                    szOutFile[256];    // Output file name
+    int                     iArgIdx;
+    FILE                  * ptOutFile;        // Output file handle
+    char                  * szTime;
+    int                     iMilliSec;
+    int                     iChannel;         // Channel number
+    int                     iBus;             // 429 bus number
+    unsigned long           lMsgs = 0;        // Total message
+    unsigned long           l429Msgs = 0;
+    int                     bVerbose;
+    int                     bDecimal;         // Hex/decimal flag
+    int                     bPrintTMATS;
+    unsigned long           ulBuffSize = 0L;
+
+    EnI106Status            enStatus;
+    SuI106Ch10Header        suI106Hdr;
+
+    unsigned char         * pvBuff  = NULL;
+    SuIrig106Time           suTime;
+	SuArinc429F0_CurrMsg    suArinc429Msg;
+    SuTmatsInfo             suTmatsInfo;
+
+/*
+ * Process the command line arguements
+ */
+
+    if (argc < 2) 
+        {
+        vUsage();
+        return 1;
+        }
+
+    iChannel        = -1;
+    iBus            = -1;
+
+    bVerbose        = bFALSE;            /* No verbosity                      */
+    bDecimal        = bFALSE;
+    bPrintTMATS     = bFALSE;
+
+    szInFile[0]  = '\0';
+    strcpy(szOutFile,"");                     // Default is stdout
+
+    for (iArgIdx=1; iArgIdx<argc; iArgIdx++) 
+        {
+
+        switch (argv[iArgIdx][0]) 
+            {
+
+            case '-' :
+                switch (argv[iArgIdx][1]) 
+                    {
+
+                    case 'v' :                   /* Verbose switch */
+                        bVerbose = bTRUE;
+                        break;
+
+                    case 'c' :                   /* Channel number */
+                        iArgIdx++;
+                        sscanf(argv[iArgIdx],"%d",&iChannel);
+                        break;
+
+                    case 'b' :                   /* Bus number */
+                        iArgIdx++;
+                        sscanf(argv[iArgIdx],"%d",&iBus);
+                        break;
+
+                    case 'T' :                   /* Print TMATS flag */
+                        bPrintTMATS = bTRUE;
+                        break;
+
+                    default :
+                        break;
+                    } /* end flag switch */
+                break;
+
+            default :
+                if (szInFile[0] == '\0') strcpy(szInFile, argv[iArgIdx]);
+                else                     strcpy(szOutFile,argv[iArgIdx]);
+                break;
+
+            } /* end command line arg switch */
+        } /* end for all arguments */
+
+    if (strlen(szInFile)==0) 
+        {
+        vUsage();
+        return 1;
+        }
+
+/*
+ * Opening banner
+ * --------------
+ */
+
+    fprintf(stderr, "\nIDMPA429 "MAJOR_VERSION"."MINOR_VERSION"\n");
+    fprintf(stderr, "Freeware Copyright (C) 2010 Irig106.org\n\n");
+
+	putenv("TZ=GMT0");
+	tzset();
+
+/*
+ *  Open file and allocate a buffer for reading data.
+ */
+
+    enStatus = enI106Ch10Open(&m_iI106Handle, szInFile, I106_READ);
+
+    switch (enStatus)
+        {
+        case I106_OPEN_WARNING :
+            fprintf(stderr, "Warning opening data file : Status = %d\n", enStatus);
+            break;
+        case I106_OK :
+            break;
+        default :
+            fprintf(stderr, "Error opening data file : Status = %d\n", enStatus);
+            return 1;
+            break;
+        }
+
+    enStatus = enI106_SyncTime(m_iI106Handle, bFALSE, 0);
+    if (enStatus != I106_OK)
+        {
+        fprintf(stderr, "Error establishing time sync : Status = %d\n", enStatus);
+        return 1;
+        }
+
+
+/*
+ * Open the output file
+ */
+
+    // If output file specified then open it    
+    if (strlen(szOutFile) != 0)
+        {
+        ptOutFile = fopen(szOutFile,"w");
+        if (ptOutFile == NULL) 
+            {
+            fprintf(stderr, "Error opening output file\n");
+            return 1;
+            }
+        }
+
+    // No output file name so use stdout
+    else
+        {
+        ptOutFile = stdout;
+        }
+
+
+/*
+ * Read the first header. If TMATS flag set, print TMATS and exit
+ */
+
+    // Read first header and check for data read errors
+    enStatus = enI106Ch10ReadNextHeader(m_iI106Handle, &suI106Hdr);
+    if (enStatus != I106_OK)
+        return 1;
+
+    // If TMATS flag set, just print TMATS and exit
+    if (bPrintTMATS == bTRUE)
+        {
+        if (suI106Hdr.ubyDataType == I106CH10_DTYPE_TMATS)
+            {
+            // Make a data buffer for TMATS
+            pvBuff = malloc(suI106Hdr.ulPacketLen);
+
+            // Read the data buffer and check for read errors
+            enStatus = enI106Ch10ReadData(m_iI106Handle, suI106Hdr.ulPacketLen, pvBuff);
+            if (enStatus != I106_OK)
+                return 1;
+
+            // Process the TMATS info
+            memset( &suTmatsInfo, 0, sizeof(suTmatsInfo) );
+            enStatus = enI106_Decode_Tmats(&suI106Hdr, pvBuff, &suTmatsInfo);
+            if (enStatus != I106_OK) 
+                {
+                fprintf(stderr, " Error processing TMATS record : Status = %d\n", enStatus);
+                return 1;
+                }
+
+            vPrintTmats(&suTmatsInfo, ptOutFile);
+            } // end if TMATS
+
+        // TMATS not first message
+        else
+            {
+            printf("Error - TMATS message not found\n");
+            return 1;
+            }
+
+        return 0;
+        } // end if print TMATS
+
+/*
+ * Read messages until error or EOF
+ */
+
+    lMsgs = 1;
+
+    while (1==1) 
+        {
+
+        // Read the next header
+        enStatus = enI106Ch10ReadNextHeader(m_iI106Handle, &suI106Hdr);
+
+        // Setup a one time loop to make it easy to break out on error
+        do
+            {
+            if (enStatus == I106_EOF)
+                break;
+
+            // Check for header read errors
+            if (enStatus != I106_OK)
+                break;
+
+            // If ARINC 429 message then process it
+            if ((suI106Hdr.ubyDataType == I106CH10_DTYPE_ARINC_429_FMT_0) &&
+                ((iChannel == -1) || (iChannel == (int)suI106Hdr.uChID)))
+                {
+
+                // Make sure our buffer is big enough, size *does* matter
+                if (ulBuffSize < suI106Hdr.ulPacketLen)
+                    {
+                    pvBuff = realloc(pvBuff, suI106Hdr.ulPacketLen);
+                    ulBuffSize = suI106Hdr.ulPacketLen;
+                    }
+
+                // Read the data buffer
+                enStatus = enI106Ch10ReadData(m_iI106Handle, ulBuffSize, pvBuff);
+
+                // Check for data read errors
+                if (enStatus != I106_OK)
+                    break;
+
+                lMsgs++;
+                if (bVerbose) 
+                    fprintf(stderr, "%8.8ld Messages \r",lMsgs);
+
+                    // Step through all ARINC 429 messages
+                enStatus = enI106_Decode_FirstArinc429F0(&suI106Hdr, pvBuff, &suArinc429Msg);
+                while (enStatus == I106_OK)
+                    {
+
+                    // If bus number specified then only print those
+                    if ((iBus == -1) || (iBus == (int)suArinc429Msg.psu429Hdr->uBusNum))
+                        {
+                        // Print out the time
+
+                        // PROBABLY REALLY OUGHT TO CHECK FOR THAT GOOFY SECONDARY
+                        // HEADER FORMAT TIME REPRESENTATION. DOES ANYONE USE THAT???
+                        enI106_RelInt2IrigTime(m_iI106Handle, suArinc429Msg.llIntPktTime, &suTime);
+                        szTime = ctime((time_t *)&suTime.ulSecs);
+					    szTime[19] = '\0';
+					    iMilliSec = (int)(suTime.ulFrac / 10000.0);
+                        fprintf(ptOutFile,"%s.%3.3d", &szTime[11], iMilliSec);
+
+                        // Print out the data
+                        fprintf(ptOutFile," %5.1u",  suI106Hdr.uChID);
+                        fprintf(ptOutFile," %3.1u",  suArinc429Msg.psu429Hdr->uBusNum);
+                        fprintf(ptOutFile," %3.1u",   suArinc429Msg.psu429Data->uLabel);
+                        fprintf(ptOutFile," %1.1u",   suArinc429Msg.psu429Data->uSDI);
+                        fprintf(ptOutFile," 0x%5.5x", suArinc429Msg.psu429Data->uData);
+                        fprintf(ptOutFile," %1.1u",   suArinc429Msg.psu429Data->uSSM);
+
+                        fprintf(ptOutFile,"\n");
+                        fflush(ptOutFile);
+
+                        l429Msgs++;
+                        if (bVerbose) printf("%8.8ld AIRNC 429 Messages \r",l429Msgs);
+                        } // end if bus number OK
+
+                    // Get the next ARINC 429 message
+                    enStatus = enI106_Decode_NextArinc429F0(&suArinc429Msg);
+
+                    } // end while processing ARINC 429 messages from an IRIG packet
+
+                } // end if ARINC 429
+
+            } while (bFALSE); // end one time loop
+
+        // If EOF break out of main read loop
+        if (enStatus == I106_EOF)
+            {
+            fprintf(stderr, "End of file\n");
+            break;
+            }
+
+        }   /* End while */
+
+/*
+ * Print out some summaries
+ */
+
+    printf("\nTotal Message %lu\n", lMsgs);
+
+
+/*
+ *  Close files
+ */
+
+    enI106Ch10Close(m_iI106Handle);
+    fclose(ptOutFile);
+
+    return 0;
+    }
+
+
+
+/* ------------------------------------------------------------------------ */
+
+void vPrintTmats(SuTmatsInfo * psuTmatsInfo, FILE * ptOutFile)
+    {
+    int                     iGIndex;
+    int                     iRIndex;
+    int                     iRDsiIndex;
+    SuGDataSource         * psuGDataSource;
+    SuRRecord             * psuRRecord;
+    SuRDataSource         * psuRDataSource;
+
+    // Print out the TMATS info
+    // ------------------------
+
+    fprintf(ptOutFile,"\n=-=-= ARINC 429 Channel Summary =-=-=\n\n");
+
+    // G record
+    fprintf(ptOutFile,"Program Name - %s\n",psuTmatsInfo->psuFirstGRecord->szProgramName);
+    fprintf(ptOutFile,"\n");
+    fprintf(ptOutFile,"Channel  Data Source         \n");
+    fprintf(ptOutFile,"-------  --------------------\n");
+
+    // Data sources
+    psuGDataSource = psuTmatsInfo->psuFirstGRecord->psuFirstGDataSource;
+    do  {
+        if (psuGDataSource == NULL) break;
+
+        // G record data source info
+        iGIndex = psuGDataSource->iDataSourceNum;
+
+        // R record info
+        psuRRecord = psuGDataSource->psuRRecord;
+        do  {
+            if (psuRRecord == NULL) break;
+            iRIndex = psuRRecord->iRecordNum;
+
+            // R record data sources
+            psuRDataSource = psuRRecord->psuFirstDataSource;
+            do  {
+                if (psuRDataSource == NULL) 
+                    break;
+                if (strcasecmp(psuRDataSource->szChannelDataType,"429IN") == 0)
+                    {
+                    iRDsiIndex = psuRDataSource->iDataSourceNum;
+                    fprintf(ptOutFile," %5i ",   psuRDataSource->iTrackNumber);
+                    fprintf(ptOutFile,"  %-20s", psuRDataSource->szDataSourceID);
+                    fprintf(ptOutFile,"\n");
+                    }
+                psuRDataSource = psuRDataSource->psuNextRDataSource;
+                } while (bTRUE);
+
+            psuRRecord = psuRRecord->psuNextRRecord;
+            } while (bTRUE);
+
+
+        psuGDataSource = psuTmatsInfo->psuFirstGRecord->psuFirstGDataSource->psuNextGDataSource;
+        } while (bTRUE);
+
+    return;
+    }
+
+
+/* ------------------------------------------------------------------------ */
+
+void vUsage(void)
+    {
+    printf("\nIDMPA429 "MAJOR_VERSION"."MINOR_VERSION" "__DATE__" "__TIME__"\n");
+    printf("Dump ARINC 429 records from a Ch 10 data file\n");
+    printf("Freeware Copyright (C) 2010 Irig106.org\n\n");
+    printf("Usage: idmpa429 <input file> <output file> [flags]\n");
+    printf("   <filename> Input/output file names        \n");
+    printf("   -v         Verbose                        \n");
+    printf("   -c ChNum   Channel Number (default all)   \n");
+    printf("   -b BusNum  429 Bus Number (default all)   \n");
+    printf("                                             \n");
+    printf("   -T         Print TMATS summary and exit   \n");
+    printf("                                             \n");
+    printf("The output data fields are:                  \n");
+    printf("Time  ChanID  BusNum  Label  SDI  Data  SSM  \n");
+    }
+
+
+
+
